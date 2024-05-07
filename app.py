afa97871afb20ae19d7eb99e19df2b7aa87d2121
Uma Mina <62588358+umamina11@users.noreply.github.com>
2024-05-07 21:52:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,63 +1,131 @@
-from flask import Flask, render_template, request, send_file
-import os
-import PyPDF2
-import io
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-def split_and_crop_pdf(input_pdf, output_folder):
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    
-    cropped_pdfs = []
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    with open(input_pdf, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-        
-        for page_num in range(len(reader.pages)):
-            page = reader.pages[page_num]
-            # Calculate the width and height of the page
-            width = page.mediabox.upper_right[0] - page.mediabox.lower_left[0]
-            height = page.mediabox.upper_right[1] - page.mediabox.lower_left[1]
+@app.route('/amazon')
+def amazon():
+    return render_template('amazon.html')
 
-            # Define the amount of space you want to leave at the bottom and top
-            space_to_leave_top = -62  # Adjust this value as needed
-            space_to_leave_bottom = 10  # Adjust this value as needed
-
-            # Set the crop box coordinates for the lower half of the page with a little space
-            cropped_page = (page.cropbox.lower_left[0], page.cropbox.lower_left[1] + height / 2 - space_to_leave_top,
-                            page.cropbox.lower_left[0] + width, page.cropbox.lower_left[1] + height - space_to_leave_bottom)
-            page.cropbox.lower_left = (cropped_page[0], cropped_page[1])
-            page.cropbox.upper_right = (cropped_page[2], cropped_page[3])
-
-            writer = PyPDF2.PdfWriter()
-            output_pdf = f"{output_folder}/page_{page_num + 1}.pdf"
-            writer.add_page(page)
-            
-            with open(output_pdf, 'wb') as output_file:
-                writer.write(output_file)
-            
-            cropped_pdfs.append(output_pdf)
-
-    return cropped_pdfs
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        input_pdf = request.files['file']
-        output_folder = request.form['output_folder']
-        
-        input_pdf_path = os.path.join('uploads', input_pdf.filename)
-        input_pdf.save(input_pdf_path)
-        
-        cropped_pdfs = split_and_crop_pdf(input_pdf_path, output_folder)
-        
-        return render_template('download.html', pdfs=cropped_pdfs)
+@app.route('/flipkart')
+def flipkart():
     return render_template('flipkart.html')
 
-@app.route('/download/<path:pdf_path>')
-def download_pdf(pdf_path):
-    return send_file(pdf_path, as_attachment=True)
+@app.route('/meesho')
+def meesho():
+    return render_template('meesho.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+@app.route('/flipkart')
+def flipkart():
+    return render_template('flipkart.html', platform='Flipkart')
+
+@app.route('/amazon')
+def amazon():
+    return render_template('amazon.html', platform='Amazon')
+
+@app.route('/meesho')
+def meesho():
+    return render_template('meesho.html', platform='Meesho')
+
+@app.route('/glassdoor')
+def glassdoor():
+    return render_template('glassdoor.html', platform='Glassdoor')
+
+
+
+
+from flask import Flask, render_template, request, redirect, url_for
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+app = Flask(__name__)
+
+# Function to crop each page of the PDF into half
+def crop_pdf_half(pdf_file):
+    pdf_reader = PdfFileReader(pdf_file)
+    pdf_writer = PdfFileWriter()
+
+    for page_num in range(pdf_reader.numPages):
+        page = pdf_reader.getPage(page_num)
+        media_box = page.mediaBox
+        media_box.lowerLeft = (media_box.getLowerLeft_x(), media_box.getLowerLeft_y())
+        media_box.upperRight = (media_box.getUpperRight_x() / 2, media_box.getUpperRight_y())
+        pdf_writer.addPage(page)
+
+    return pdf_writer
+
+# Function to split all pages of the PDF into halves
+def split_pdf_half(pdf_file):
+    pdf_reader = PdfFileReader(pdf_file)
+    pdf_writer = PdfFileWriter()
+
+    for page_num in range(pdf_reader.numPages):
+        page = pdf_reader.getPage(page_num)
+        media_box = page.mediaBox
+        width = media_box.getUpperRight_x() - media_box.getLowerLeft_x()
+        height = media_box.getUpperRight_y() - media_box.getLowerLeft_y()
+
+        half_width = width / 2
+        half_height = height / 2
+
+        for x in range(2):
+            for y in range(2):
+                new_page = pdf_writer.addBlankPage(half_width, half_height)
+                new_page.mergeTranslatedPage(page, -x * half_width, -y * half_height)
+
+    return pdf_writer
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'pdf_file' not in request.files:
+        return redirect(request.url)
+
+    pdf_file = request.files['pdf_file']
+    if pdf_file.filename == '':
+        return redirect(request.url)
+
+    if pdf_file:
+        # Crop each page into half
+        cropped_pdf_half = crop_pdf_half(pdf_file)
+        cropped_half_filename = 'cropped_half.pdf'
+        with open(cropped_half_filename, 'wb') as output_pdf:
+            cropped_pdf_half.write(output_pdf)
+
+        # Split all pages into halves
+        split_pdf = split_pdf_half(pdf_file)
+        split_half_filename = 'split_half.pdf'
+        with open(split_half_filename, 'wb') as output_pdf:
+            split_pdf.write(output_pdf)
+
+        return render_template('result.html', 
+                                cropped_half_filename=cropped_half_filename,
+                                split_half_filename=split_half_filename)
+
+@app.route('/flipkart')
+def flipkart():
+    return render_template('flipkart.html')
+
+@app.route('/amazon')
+def amazon():
+    return render_template('amazon.html')
+
+@app.route('/meesho')
+def meesho():
+    return render_template('meesho.html')
+
+@app.route('/glassdoor')
+def glassdoor():
+    return render_template('glassdoor.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
